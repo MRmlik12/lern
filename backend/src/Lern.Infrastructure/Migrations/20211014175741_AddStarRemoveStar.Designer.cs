36d18f0f27613f46078e27b1fb7ffefb2f314ddb
MRmlik12 <mrmlik12sup@gmail.com>
2021-10-14 19:58:46 +0200

    Create migration for this pr

--- backend/src/Lern.Infrastructure/Migrations/20211014175741_AddStarRemoveStar.Designer.cs
+++ backend/src/Lern.Infrastructure/Migrations/20211014175741_AddStarRemoveStar.Designer.cs
@@ -5,15 +5,17 @@ using Lern.Core.ProjectAggregate.Set;
 using Lern.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Lern.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211014175741_AddStarRemoveStar")]
+    partial class AddStarRemoveStar
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/src/Lern.Infrastructure/Migrations/20211014175741_AddStarRemoveStar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lern.Infrastructure.Migrations
+{
+    public partial class AddStarRemoveStar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Stars",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Stars",
+                table: "Users",
+                column: "Stars");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Sets_Stars",
+                table: "Users",
+                column: "Stars",
+                principalTable: "Sets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Sets_Stars",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Stars",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Stars",
+                table: "Users");
+        }
+    }
+}
