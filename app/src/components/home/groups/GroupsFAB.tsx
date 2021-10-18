4406d62804795dc403e35543c10f785b10cf37a0
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2021-10-18 13:01:11 +0200

    Add join group dialog (#22)

--- app/src/components/home/groups/GroupsFAB.tsx
+++ app/src/components/home/groups/GroupsFAB.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { FAB, Portal, Provider } from "react-native-paper";
 import CreateGroup from "../../../views/home/bottom/groups/CreateGroup";
+import JoinGroup from "../../../views/home/bottom/groups/JoinGroup";
 
 const GroupsFAB = () => {
   const [state, setState] = React.useState({ open: false });
   const [createGroupVisible, setCreateGroupVisible] = React.useState(false);
+  const [joinGroupVisible, setJoinGroupVisible] = React.useState(false);
   const onStateChange = ({ open }: any) => setState({ open });
 
   const fabActions = [
@@ -16,13 +18,14 @@ const GroupsFAB = () => {
     {
       icon: "account-multiple-plus-outline",
       label: "Join to group",
-      onPress: () => console.log(""),
+      onPress: () => setJoinGroupVisible(true),
     },
   ];
 
   return (
     <Provider>
       {createGroupVisible ? <CreateGroup /> : null}
+      {joinGroupVisible ? <JoinGroup /> : null}
       <Portal>
         <FAB.Group
           visible={true}
--- app/src/views/home/bottom/groups/JoinGroup.tsx
+++ app/src/views/home/bottom/groups/JoinGroup.tsx
@@ -1,13 +1,62 @@
 import React from "react";
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { ToastAndroid, View } from "react-native";
+import {
+  Button,
+  Dialog,
+  Portal,
+  Provider,
+  TextInput,
+} from "react-native-paper";
+import { joinGroup } from "../../../../api/apiClient";
+import { isFalsy } from "utility-types";
 
-const CreateGroup: React.FC = () => {
+const JoinGroup: React.FC = () => {
+  const [visible, setVisible] = React.useState(true);
+  const [isError, setIsError] = React.useState(false);
+  const [groupCode, setGroupCode] = React.useState("");
+
+  const hideDialog = () => setVisible(false);
+
+  const handleJoinGroup = async () => {
+    if (isFalsy(groupCode)) {
+      setIsError(true);
+      return;
+    }
+
+    const isJoined = await joinGroup(groupCode);
+    if (isJoined) {
+      hideDialog();
+      return;
+    }
+
+    ToastAndroid.show("Invalid message", ToastAndroid.SHORT);
+  };
+
   return (
-    <View>
-      <Text>DAAAA</Text>
-    </View>
+    <Provider>
+      <View>
+        <Portal>
+          <Dialog visible={visible} onDismiss={hideDialog}>
+            <Dialog.Title>Join to group</Dialog.Title>
+            <Dialog.Content>
+              <TextInput
+                label="Code"
+                value={groupCode}
+                error={isError}
+                mode="outlined"
+                maxLength={10}
+                onChangeText={(text) => setGroupCode(text)}
+              />
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button onPress={hideDialog}>Cancel</Button>
+              <Button onPress={handleJoinGroup}>Join</Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
+      </View>
+    </Provider>
   );
 };
 
-export default CreateGroup;
+export default JoinGroup;
